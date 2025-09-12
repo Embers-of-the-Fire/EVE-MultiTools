2293d6b129847daf025ba42a97614419c5e75a74
Embers-of-the-Fire <stellarishs@163.com>
2025-09-12 16:11:08 +0800

    rm: remove unused cards

--- src/components/card/universe-object/index.ts
+++
@@ -1,4 +0,0 @@
-export { ConstellationCard } from "./ConstellationCard";
-export { PlanetCard } from "./PlanetCard";
-export { RegionCard } from "./RegionCard";
-export { SystemCard } from "./SystemCard";
